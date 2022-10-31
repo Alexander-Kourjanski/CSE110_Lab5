e166cff65ff214d0cc528d404f93ccb3296cd855
Alexander-Kourjanski <sashakrj@gmail.com>
2022-10-30 18:08:47 -0700

    Update explore.js
    Added menu and trying to make the button work

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -5,3 +5,20 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
 }
+
+const languages = document.getElementById("voice-select");
+languages.addEventListener('click', (language) =>{
+  const voices = speechSynthesis.getVoices();
+  console.log(voices);
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+    document.getElementById("voice-select").append(option);
+  }
+});
+
+const button = document.querySelector("button");
+button.addEventListener('click', (speech) => {
+  const speek = window.SpeechSynthesis;
+  speek.speak(document.getElementById("rext-to-speak"));
+});
