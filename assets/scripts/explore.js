7916cfaddc104bf72ce39a7c88746c2fa4c78eec
dziper <dziper@me.com>
2022-10-30 20:55:45 -0700

    finish javascript part 2

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,24 +1,57 @@
 // explore.js
-
+const synth = window.speechSynthesis;
 window.addEventListener('DOMContentLoaded', init);
+let voices = [];
 
 function init() {
-  // TODO
+  // const voices = synth.getVoices();
+  // console.log(voices);
+  // for (let i = 0; i < voices.length; i++) {
+  //   const option = document.createElement('option');
+  //   option.textContent = `${voices[i].name} (${voices[i].lang})`;
+    
+  // }
+
+  const button = document.querySelector("button");
+  button.addEventListener('click', sayTextBox);
 }
 
-const languages = document.getElementById("voice-select");
-languages.addEventListener('click', (language) =>{
-  const voices = speechSynthesis.getVoices();
-  console.log(voices);
-  for (let i = 0; i < voices.length; i++) {
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length ; i++) {
     const option = document.createElement('option');
     option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+    if (voices[i].default) {
+      option.textContent += ' — DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
     document.getElementById("voice-select").append(option);
   }
-});
+}
 
-const button = document.querySelector("button");
-button.addEventListener('click', (speech) => {
-  const speek = window.SpeechSynthesis;
-  speek.speak(document.getElementById("rext-to-speak"));
-});
+setTimeout(populateVoiceList, 10);
+
+function sayTextBox() {
+  let utterance = new SpeechSynthesisUtterance(document.getElementById("text-to-speak").value);
+  
+  const selectedOption = document.getElementById("voice-select").selectedOptions[0].getAttribute('data-name');
+  for (let i = 0; i < voices.length ; i++) {
+    if (voices[i].name === selectedOption) {
+      utterance.voice = voices[i];
+    }
+  }
+  
+  const image = document.querySelector("img");
+  image.src = "assets/images/smiling-open.png";
+
+  console.log("Start speaking");
+  synth.speak(utterance);
+  utterance.addEventListener('end', (event) => {
+    console.log("Done speaking");
+    image.src = "assets/images/smiling.png";
+  });
+}
